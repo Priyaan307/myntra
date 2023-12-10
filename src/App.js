f306d5e4dd960c0c8ecdb0837f35a9428dca9bc1
priyadarshani <priyarrsdce@gmail.com>
2023-12-10 23:21:39 +0530

    Day53

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Home/Navbar';
 import HomePage from './Pages/HomePage';
+import ProductPage from './Pages/ProductPage';
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+      <Navbar/>
+      {/* <HomePage/> */}
+<ProductPage/>
     </div>
   );
 }
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Navbar from "../Components/Home/Navbar";
+
 import Banner from "../Components/Home/Banner";
 
 const HomePage = () => {
   return (
     <div>
-      <Navbar />
+      
       <Banner/>
     </div>
   );
